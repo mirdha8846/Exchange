7628fa8e41a12b2cd297ab3f023aa539ee3df74e
Pankaj <pankaj.mirdha22b@iiitg.ac.in>
2025-07-18 12:23:25 +0530

    working fine

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -4,14 +4,19 @@ mod utils;
 mod routes;
 mod middleware;
 use axum::{
-    routing::{get,post},
-    Router
+    middleware::from_fn, routing::{get,post}, Router
 };
+use middleware::auth_middleware;
 use routes::{login_handler};
-
+use tower::{ServiceBuilder};
+use tower_http::{trace::TraceLayer};
 #[tokio::main]
 async fn main(){
+let middlwears=ServiceBuilder::new().check_clone()
+                                                            .layer(TraceLayer::new_for_http());
+                                                            
 let app=Router::new().route("/login", post(login_handler));
+                            //  .layer(from_fn(auth_middleware));
 let tcplister=tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
-axum::serve(tcplister, app);
+axum::serve(tcplister, app).await.unwrap();
 }
